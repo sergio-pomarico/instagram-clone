709446a87c9d76049247e2113027c618185b37eb
Sergio Pomarico <sergio@tyba.com.co>
2019-08-22 00:21:29 -0500

    install react-native-vector-icons

--- android/app/src/main/java/com/instagram_clone/MainApplication.java
+++ android/app/src/main/java/com/instagram_clone/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
             new LinearGradientPackage(),
             new RNGestureHandlerPackage(),
             new RNFirebaseAuthPackage(),
-            new SplashScreenReactPackage()
+            new SplashScreenReactPackage(),
+            new VectorIconsPackage()
       );
     }
 
